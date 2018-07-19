3583ee422724e117a2ff8941d8ffe82df7a20c9c
Eduardo Ludi <eduludi@gmail.com>
2018-07-19 10:50:15 +0200

    Add a tests for invalid validator provided to valdiate()

--- src/validate.test.js
+++ src/validate.test.js
@@ -5,6 +5,10 @@ test('with null', () => {
   expect(validate(null)).toBe(undefined)
 })
 
+test('with an "invalid" validator', () => {
+  expect(() => validate('test@email.com', { invalid: true })).toThrow()
+})
+
 test('with email rule', () => {
   expect(validate('test@email.com', { email: true })).toBe(undefined)
   expect(validate('test@com', { email: true })).toEqual([defaults.message])
