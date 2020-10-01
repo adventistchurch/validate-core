76b2aff4aa171b88f6a658477c8690f8ec6a2df5
Eduardo Ludi <eduludi@gmail.com>
2020-10-01 13:16:21 +0200

    Updates dev dependencies (and rollup.config.js)
    - @babel/core 7.11.6
    - @babel/preset-env 7.11.5
    - @rollup/plugin-babel 5.2.1
    - babel-jest 26.3.0
    - eslint 7.10.0
    - jest 26.4.2
    - regenerator-runtime 0.13.7
    - rollup 2.28.2
    - rollup-plugin-filesize 9.0.2
    - rollup-plugin-terser 7.0.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import { terser } from 'rollup-plugin-terser'
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import filesize from 'rollup-plugin-filesize'
 
 import { name as libraryName } from './package.json'
@@ -7,17 +7,18 @@ import { name as libraryName } from './package.json'
 const babelConf = {
   babelrc: false,
   presets: [['@babel/preset-env', { modules: false }]],
-  exclude: 'node_modules/**'
+  exclude: 'node_modules/**',
+  babelHelpers: 'bundled',
 }
 
 const outputs = [
   {
-    fileExt: '.js'
+    fileExt: '.js',
   },
   {
     fileExt: '.min.js',
-    plugins: [terser()]
-  }
+    plugins: [terser()],
+  },
 ]
 
 export default outputs.map(({ fileExt, plugins = [] }) => ({
@@ -25,7 +26,7 @@ export default outputs.map(({ fileExt, plugins = [] }) => ({
   output: {
     file: 'lib/' + libraryName + fileExt,
     format: 'cjs',
-    sourcemap: true
+    sourcemap: true,
   },
-  plugins: [babel(babelConf), ...plugins, filesize()]
+  plugins: [babel(babelConf), ...plugins, filesize()],
 }))
